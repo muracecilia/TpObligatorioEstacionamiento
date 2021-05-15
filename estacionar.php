11d6488adbca37f23c55944d048ba8f37d0db3a8
cecilia <muracecilia@gmail.com>
2021-05-14 22:05:17 -0300

    estacionar1

--- estacionar.php
+++ estacionar.php
@@ -108,7 +108,7 @@
   <footer class="pt-4 my-md-5 pt-md-5 border-top">
     <div class="row">
       <div class="col-12 col-md">
-        <img class="mb-2" src="../assets/brand/bootstrap-solid.svg" alt="" width="24" height="24">
+        <img class="mb-2" src="https://uxwing.com/wp-content/themes/uxwing/download/07-design-and-development/bootstrap-4.png" alt="" width="24" height="24">
         <small class="d-block mb-3 text-muted">&copy; 2017-2021</small>
       </div>
       <div class="col-6 col-md">
